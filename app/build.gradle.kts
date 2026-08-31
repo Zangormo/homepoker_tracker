b4df4a391042c9c762dc0b8dbd48799dd8514c62
Ilja Birjukov <zangormo@gmail.com>
2026-08-31 20:09:42 +0300

    Version change to 1.2 beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "Beta 1.1"
+        versionName = "Beta 1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
